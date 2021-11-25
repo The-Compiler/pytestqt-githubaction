08b1e1c86ac4668d430733b197d401fe13d8027f
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-11-25 23:28:20 +0900

    add cv2 import

--- test_qtbot.py
+++ test_qtbot.py
@@ -1,4 +1,8 @@
+import cv2
 from PyQt5.QtWidgets import QLabel
+
+# In my original project, cv2 was used to read an image as numpy array.
+# The array was then converted to pixmap and updated to QLabel.
 
 def test_QLabel(qtbot):
     label = QLabel()
