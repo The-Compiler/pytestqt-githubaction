a41f038daf9ac310fbd69307501b9f1fee75b6e8
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-11-25 23:58:20 +0900

    Lazy-import cv2

--- test_qtbot.py
+++ test_qtbot.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import QLabel
-import cv2
 
 # In my original project, cv2 was imported to read an image as numpy
 # array. The array was then converted to pixmap and updated to QLabel,
 # which was tested.
 
 def test_QLabel(qtbot):
+    import cv2
     label = QLabel()
     label.show()
     qtbot.addWidget(label)
