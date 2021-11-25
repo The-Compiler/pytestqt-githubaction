a47003dc57761e11da413f239bb06e9c0bca6fb8
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-11-25 23:17:12 +0900

    make test_qtbot more real

--- test_qtbot.py
+++ test_qtbot.py
@@ -1,2 +1,10 @@
-def test_qtbot(qtbot):
-    assert True
+import cv2
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import (QHBoxLayout, QLabel, QPushButton, QSizePolicy,
+    QSlider, QStyle, QVBoxLayout, QWidget)
+
+def test_QLabel(qtbot):
+    label = QLabel()
+    label.show()
+    qtbot.addWidget(label)
