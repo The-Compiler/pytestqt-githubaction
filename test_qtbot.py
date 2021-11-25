976f3af95be269c594f5a290a0f7657e772e0524
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-11-25 23:31:51 +0900

    move "import cv2" into function block

--- test_qtbot.py
+++ test_qtbot.py
@@ -1,10 +1,11 @@
-import cv2
 from PyQt5.QtWidgets import QLabel
 
-# In my original project, cv2 was used to read an image as numpy array.
-# The array was then converted to pixmap and updated to QLabel.
+# In my original project, cv2 was imported to read an image as numpy
+# array. The array was then converted to pixmap and updated to QLabel,
+# which was tested.
 
 def test_QLabel(qtbot):
+    import cv2
     label = QLabel()
     label.show()
     qtbot.addWidget(label)
